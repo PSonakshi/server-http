b1eb4d1dd7e2d7eccd8a4556de13faf62fdf878b
psonakshi <sonakshii1806@gmail.com>
2024-12-19 22:33:32 +0530

    second commit

--- server.js
+++ server.js
@@ -9,19 +9,22 @@ const server = http.createServer((req, res) => {
     if (url === '/') {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
-        return res.end('Welcome to my Server');
+        res.end('Welcome to my Server');
+        return;
     }
 
     if (url === '/text' && method === 'GET') {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
-        return res.end('Why did the backend developer break up with the frontend developer? Because they couldn\'t handle the requests! ');
+        res.end('Why did the backend developer break up with the frontend developer? Because they couldn\'t handle the requests! ');
+        return;
     }
 
     if (url === '/html' && method === 'GET') {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/html');
-        return res.end('<html><body><h1>This is an HTML page</h1></body></html>');
+        res.end('<html><body><h1>This is an HTML page</h1></body></html>');
+        return;
     }
 
     const mediaRoutes = {
@@ -38,13 +41,15 @@ const server = http.createServer((req, res) => {
 
         res.statusCode = 200;
         res.setHeader('Content-Type', contentType);
-        return res.end('Media content here');
+        res.end('Media content here');
+        return;
     }
 
     if (url === '/pdf' && method === 'GET') {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/pdf');
-        return res.end('PDF content here');
+        res.end('PDF content here');
+        return;
     }
 
     res.statusCode = 404;
